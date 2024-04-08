f87ad0cf7779b6e324de27f1c7279f18534c53fd
Ruwan Pradeep <140706867+RuwanPradeep0@users.noreply.github.com>
2024-04-08 15:32:46 +0530

    Add files via upload

---
+++ backend/VenueVista/src/main/java/com/VenueVista/VenueVista/VenueVistaApplication.java
@@ -0,0 +1,13 @@
+package com.VenueVista.VenueVista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenueVistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenueVistaApplication.class, args);
+	}
+
+}
