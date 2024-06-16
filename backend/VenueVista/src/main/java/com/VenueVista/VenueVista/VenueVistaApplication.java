393c084754bdf299ca5cdbbdeabbebfbfae07f81
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-06-17 00:17:18 +0530

    cleaning the code

--- backend/VenueVista/src/main/java/com/VenueVista/VenueVista/VenueVistaApplication.java
+++ backend/VenueVista/src/main/java/com/VenueVista/VenueVista/VenueVistaApplication.java
@@ -3,8 +3,6 @@ package com.VenueVista.VenueVista;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 @SpringBootApplication
 @EnableScheduling
