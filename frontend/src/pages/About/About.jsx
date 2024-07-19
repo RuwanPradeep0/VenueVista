ea82b8e5455bc639517b5dee6aeb9304a5366047
Dinusha Sandamali <155517342+dinushasandamali@users.noreply.github.com>
2024-07-19 12:52:25 +0530

    About

---
+++ frontend/src/pages/About/About.jsx
@@ -0,0 +1,14 @@
+// src/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to VenueVista! We are dedicated to helping you manage and book lecture halls with ease.</p>
+      {/* You can add more information about your project or team here */}
+    </div>
+  );
+};
+
+export default About;
