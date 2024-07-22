24904ffe118fabf850d0ee0fa7d810678a71d3d2
Dinusha Sandamali <155517342+dinushasandamali@users.noreply.github.com>
2024-07-23 00:48:00 +0530

    About updated

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -6,71 +6,87 @@ import image02 from '../../images/bookingpage.png'
 import image03 from '../../images/bookhall.png'
 import image04 from '../../images/waitingreservation.png'
 import image05 from '../../images/addwaiting.png'
+import image06 from '../../images/filtering.png'
 import image07 from '../../images/addhall.png'
 import { IoIosArrowDown } from "react-icons/io";
+import Hero from '../../components/Hero';
 
 
 const About = () => {
 
   const [visibleStep, setVisibleStep] = useState(null);
 
-  const toggleVisibility = (step) => {
-    setVisibleStep(visibleStep === step ? null : step);
-  };
+
+const toggleVisibility = (index) => {
+  setVisibleStep(visibleStep === index ? null : index);
+};
 
   const steps = [
     {
       title: 'User Registration or Login',
-      content: 'Only Lecturers and instructors are permitted to register; students are not allowed.<br />Access to functionalities is granted only after successful login.<br />Authentication mechanisms such as username/password or two-factor authentication may be employed.',
+      steps: [
+        'Only Lecturers and instructors are permitted to register but Students are not allowed.',
+        'Access to functionalities is granted only after successful login.',
+        'Authentication mechanisms such as username/password or two-factor authentication may be employed.',
+      ],
       image: image01,
-      alt: 'User Registration'
+      alt: 'User Registration',
     },
     {
       title: 'View Reservation Calendar',
-      content: 'Lecturers and possibly Students shall be able to view a calendar displaying booked and available time slots for lecture halls.<br />The calendar should provide an overview of reservations for efficient scheduling.',
+      steps:[ 'Lecturers and possibly Students shall be able to view a calendar displaying booked and available time slots for lecture halls.',
+             'The calendar should provide an overview of reservations for efficient scheduling.',
+      ],
       image: image02,
-      alt: 'View Reservation Calendar'
+      alt: 'View Reservation Calendar',
     },
     {
       title: 'Book Time Slots',
-      content: 'Lecturers should be able to book available time slots for lecture halls.<br />The booking process should allow selection of date, time slot, and any necessary infrastructure requirements (e.g., projectors, whiteboards).',
+      steps: ['Lecturers should be able to book available time slots for lecture halls.',
+        'The booking process should allow selection of date, time slot, and any necessary infrastructure requirements.',],
       image: image03,
-      alt: 'Book Time Slots'
+      alt: 'Book Time Slots',
     },
     {
       title: 'Manage Reservation',
-      content: 'Users who booked a time slot should be able to cancel reservations.<br />Upon cancellation, the system should notify users on the waiting list, if any, about the newly available time slot.',
+      steps: ['Users who booked a time slot should be able to cancel reservations.',
+        'Upon cancellation, the system should notify users on the waiting list, if any, about the newly available time slot.',],
       image: image04,
-      alt: 'Manage Reservation'
+      alt: 'Manage Reservation',
     },
     {
       title: 'Waiting List',
-      content: 'Lecturers and possibly administrative staff should be able to view reservations on the waiting list.<br />The system should automatically notify users on the waiting list when a previously booked time slot becomes available.',
+      steps: ['Lecturers and possibly administrative staff should be able to view reservations on the waiting list.',
+        'The system should automatically notify users on the waiting list when a previously booked time slot becomes available.',],
       image: image05,
-      alt: 'Waiting List'
+      alt: 'Waiting List',
     },
     {
       title: 'Filtering Facilities',
-      content: 'Lecturers should be able to filter available time slots based on specific facilities and infrastructures required for their lecture (e.g., projector availability, seating capacity).',
+      steps: 'Lecturers should be able to filter available time slots based on specific facilities and infrastructures required for their lecture (e.g., projector availability, seating capacity).',
       image: image02,
-      alt: 'Filtering Facilities'
+      alt: 'Filtering Facilities',
     },
     {
       title: 'Adding/Updating Lecture Halls',
-      content: 'Only authorized administrative staff (e.g., MA) should be able to add or update lecture halls within the system.<br />This functionality includes adding new lecture halls, updating existing ones, and ensuring their availability for reservation.',
+      steps: ['Only authorized administrative staff (e.g., MA) should be able to add or update lecture halls within the system.',
+        'This functionality includes adding new lecture halls, updating existing ones, and ensuring their availability for reservation.',],
       image: image07,
-      alt: 'Adding/Updating Lecture Halls'
-    }
+      alt: 'Adding/Updating Lecture Halls',
+    },
   ];
-
-
+  
   return (
-    <div className="about-page">
-      <section className='topic'>
-        <div className='topic-text'>
-        <h1>About VenueVista</h1> 
-        </div>
-      </section>
+
+    <div>
+        <Hero
+                spanText="About"
+                title="VenueVista"
+                description="Effortlessly Book and Manage Lecture Halls with Ease."
+            />
+
+<div className="about-page">
+      
       <section className="hero-section">
         <div className="hero-text">
           
@@ -83,21 +99,25 @@ const About = () => {
       <h2>User Guide</h2>
       {steps.map((step, index) => (
         <div key={index} className="guide-step">
-          <img src={step.image} alt={step.alt} />
+          <img src={step.image} alt={step.alt} className="guide-image" />
           <div className="guide-text">
-            <h3 onClick={() => toggleVisibility(index)}>
-              {step.title}
-              <span className={`arrow ${visibleStep === index ? 'up' : 'down'}`}>
-                <IoIosArrowDown className={visibleStep === index ? 'rotate-up' : 'rotate-down'} />
-              </span>
-            </h3>
-            {visibleStep === index && (
-              <div className="step-content">
-                {step.content.split('\n').map((content, contentIndex) => (
-                  <p key={contentIndex} className="description-box" dangerouslySetInnerHTML={{ __html: content }}></p>
-                ))}
-              </div>
-            )}
+            <div className="text-container">
+              <h3 onClick={() => toggleVisibility(index)}>
+                {step.title}
+                <span className={`arrow ${visibleStep === index ? 'up' : 'down'}`}>
+                  <IoIosArrowDown className={visibleStep === index ? 'rotate-up' : 'rotate-down'} />
+                </span>
+              </h3>
+              {visibleStep === index && (
+                <div className="step-content">
+                  {step.steps.map((content, contentIndex) => (
+                    <p key={contentIndex} className="description-box">
+                      {content}
+                    </p>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       ))}
@@ -111,7 +131,10 @@ const About = () => {
     <p>Phone: (+94) 36456-7890</p>
 </footer>
     </div>
+    </div>
+   
   );
 };
+
 
 export default About;
